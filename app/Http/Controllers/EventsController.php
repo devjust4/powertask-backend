32bbe0ed7a3d10255cb46792a2af4a1f20c80eec
Dani <danixidev@gmail.com>
2022-02-02 12:01:04 +0100

    FInish editing and deleting events

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -14,7 +14,7 @@ class EventsController extends Controller
             try {
                 $validator = Validator::make(json_decode($data, true), [
                     'name' => 'required|string',
-                    'type' => 'required|in:exam,doctor',
+                    'type' => 'required|in:exam,medical,vacations',
                     'date_start' => 'required|date_format:Y-m-d',
                     'date_end' => 'required|date_format:Y-m-d',
                     'subject_id' => 'required|integer|exists:subjects,id',
@@ -48,5 +48,64 @@ class EventsController extends Controller
         } else {
             return response(null, 412);     //Ran when received data is empty    (412: Precondition failed)
         }
+    }
+    public function edit(Request $request, $id) {
+        $data = $request->getContent();
+        if($data) {
+            try {
+                $validator = Validator::make(json_decode($data, true), [
+                    'name' => 'string',
+                    'type' => 'in:exam,medical,vacations',
+                    'date_start' => 'date_format:Y-m-d',
+                    'date_end' => 'date_format:Y-m-d',
+                    'subject_id' => 'integer|exists:subjects,id',
+                ]);
+
+                if (!$validator->fails()) {
+                    $data = json_decode($data);
+
+                    if($event = Event::find($id)) {
+                        if(isset($data->name)) $event->name = $data->name;
+                        if(isset($data->type)) $event->type = $data->type;
+                        if(isset($data->date_start)) $event->date_start = $data->date_start;
+                        if(isset($data->date_end)) $event->date_end = $data->date_end;
+                        if(isset($data->subject_id)) $event->subject_id = $data->subject_id;
+
+                        $event->save();
+
+                        $response['response'] = "Event edited properly";
+                        $http_status_code = 200;
+                    } else {
+                        $response['response'] = "Event by that id doesn't exist.";
+                        $http_status_code = 404;
+                    }
+                } else {
+                    $response = ['status'=>0, 'msg'=>$validator->errors()->first()];
+                    $http_status_code = 400;
+                }
+            } catch (\Throwable $th) {
+                $response['response'] = "An error has occurred: ".$th->getMessage();
+                $http_status_code = 500;
+            }
+            return response()->json($response)->setStatusCode($http_status_code);
+        } else {
+            return response(null, 412);     //Ran when received data is empty    (412: Precondition failed)
+        }
+    }
+    public function delete(Request $request, $id) {
+        try {
+            if ($event = Event::find($id)) {
+                $event->delete();
+                $response['response'] = "Event deleted successfully.";
+                $http_status_code = 200;
+            } else {
+                $response['response'] = "Event by that id doesn't exist.";
+                $http_status_code = 404;
+            }
+        } catch (\Throwable $th) {
+            $response['response'] = "An error has occurred: ".$th->getMessage();
+            $http_status_code = 500;
+        }
+        return response()->json($response)->setStatusCode($http_status_code);
     }
 }
