74cc03a15b2f64e5ba35874804a0dba8d3b23e29
Dani <92784534+danixidev@users.noreply.github.com>
2022-02-07 10:13:41 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,7 @@ Route::get('/auth/redirect', function () {
 Route::get('/auth/callback', function (Request $request) {
     $response['msg'] = 'Peticion redirigida correctamente';
 
-    $response['token'] = Socialite::driver('google')->user()->token;
+    $response['token'] = Socialite::driver('google')->user();
 
     return response()->json($response);
 });
