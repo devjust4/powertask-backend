f7954d8e96871e58da8ab7e09150e7d616bc53f1
Dani <92784534+danixidev@users.noreply.github.com>
2022-02-07 14:12:46 +0100

    Make unique email and google_id

--- database/migrations/2022_01_18_183753_create_students_table.php
+++ database/migrations/2022_01_18_183753_create_students_table.php
@@ -16,9 +16,9 @@ class CreateStudentsTable extends Migration
         Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email');
+            $table->string('email')->unique();
             $table->string('image_url');
-            $table->string('google_id');
+            $table->string('google_id')->unique();
             $table->timestamps();
         });
     }
