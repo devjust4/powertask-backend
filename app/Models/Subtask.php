a26dacfdfded5281d5770ceb5dd3363d6d2c4e30
Dani <92784534+danixidev@users.noreply.github.com>
2022-02-14 18:11:33 +0100

    Hide extra values

--- app/Models/Subtask.php
+++ app/Models/Subtask.php
@@ -9,6 +9,12 @@ class Subtask extends Model
 {
     use HasFactory;
 
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'task_id',
+    ];
+
     public function task() {
         return $this->belongsTo(Task::class);
     }
