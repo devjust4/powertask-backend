a75b17a05fd24f5b0061a2887aacaccf1898451e
Dani <92784534+danixidev@users.noreply.github.com>
2022-03-01 21:23:21 +0100

    Change initial download with events' and tasks' changes

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -77,7 +77,7 @@ class StudentsController extends Controller
                     $events_array = array();
 
                     if(!$subjects->isEmpty()) {
-                        if(!$student->subjects()->where('google_id', '<>', null)->get()->isEmpty()) {
+                        if($student->subjects()->where('google_id', '<>', null)->first()) {
                             $client = new \Google\Client();
                             $client->setAuthConfig('../laravel_id_secret.json');
                             $client->addScope('https://www.googleapis.com/auth/classroom.course-work.readonly');
@@ -86,9 +86,7 @@ class StudentsController extends Controller
 
                             $service = new Classroom($client);
                             foreach ($subjects as $subject) {
-                                if($subject->google_id != null) {
-                                    $google_tasks = $service->courses_courseWork->listCoursesCourseWork($subject->google_id)->courseWork;
-                                }
+                                $google_tasks = $service->courses_courseWork->listCoursesCourseWork($subject->google_id)->courseWork;
                             }
 
                             foreach ($google_tasks as $google_task) {
@@ -100,10 +98,13 @@ class StudentsController extends Controller
                                     $task = new Task();
                                     $task->student_id = $request->student->id;
                                     $task->google_id = $google_task->id;
+                                    $task->subject_id = Subject::where('google_id', $google_task->courseId)->first()->id;
 
                                     $task->name = $google_task->title;
                                     if($google_task->description) $task->description = $google_task->description;
-                                    if($google_task->dueDate) $task->date_handover = $google_task->dueDate->year.'-'.$google_task->dueDate->month.'-'.$google_task->dueDate->day;
+                                    if($google_task->dueDate) {
+                                        $task->date_handover = strtotime($google_task->dueDate->year.'-'.$google_task->dueDate->month.'-'.$google_task->dueDate->day);
+                                    }
 
                                     if($submission->assignedGrade) $task->mark = $submission->assignedGrade;
 
@@ -122,7 +123,9 @@ class StudentsController extends Controller
                                 } else {
                                     $task_ref->name = $google_task->title;
                                     if($google_task->description) $task_ref->description = $google_task->description;
-                                    if($google_task->dueDate) $task_ref->date_handover = $google_task->dueDate->year.'-'.$google_task->dueDate->month.'-'.$google_task->dueDate->day;
+                                    if($google_task->dueDate) {
+                                        $task_ref->date_handover = strtotime($google_task->dueDate->year.'-'.$google_task->dueDate->month.'-'.$google_task->dueDate->day);
+                                    }
                                     if($google_task->description) $task_ref->description = $google_task->description;
 
                                     if($submission->assignedGrade) $task_ref->mark = $submission->assignedGrade;
@@ -158,35 +161,15 @@ class StudentsController extends Controller
                         }
                     }
 
-                    if(!$student->events()->get()->isEmpty()) {
-                        $first_date = $student->events()->orderBy('date_start', 'asc')->first()->date_start;        //Recojo la primera fecha que haya
-                        $begin = new DateTime($first_date);
+                    $events = $student->events()->orderBy('timestamp_start', 'asc')->get();
 
-                        $last_date = $student->events()->orderBy('date_end', 'desc')->first()->date_end;            //Recojo la ultima fecha que haya
-                        $end = new DateTime($last_date);
-                        $end->modify('+1 day');
-
-                        $interval = DateInterval::createFromDateString('1 day');
-                        $period = new DatePeriod($begin, $interval, $end);
-
-                        foreach ($period as $date) {                //Recorro todo ese intervalo
-                            $date = $date->format("Y-m-d");
-
-                            $events = $student->events()->where('date_start', '<=', $date)->where('date_end', '>=', $date)->get();
-                            if(!$events->isEmpty()) {
-                                $array["vacation"] = array();
-                                $array["exam"] = array();
-                                $array["personal"] = array();
-
-                                foreach ($events as $event) {
-                                    if($event->all_day == true) $event->makeHidden(['time_start', 'time_end']);
-
-                                    if($event->type == "vacation") array_push($array["vacation"], $event);
-                                    if($event->type == "exam") array_push($array["exam"], $event);
-                                    if($event->type == "personal") array_push($array["personal"], $event);
-                                }
-                                $events_array[strtotime($date)] = $array;
-                            }
+                    if(!$events->isEmpty()) {
+                        foreach ($events as $event) {
+                            $events_array[$event->id] = $event;
+                        }
+                        if($events_array) {
+                            $response['events'] = $events_array;
+                            $http_status_code = 200;
                         }
                     }
 
