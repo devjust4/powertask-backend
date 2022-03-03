7ff19b8050ac374c667e6999f4740cf05e320dc0
Dani <92784534+danixidev@users.noreply.github.com>
2022-03-03 20:45:10 +0100

    Make completed tasks widget

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -324,8 +324,19 @@ class StudentsController extends Controller
             $student = Student::find($request->student->id);
             $tasks = $student->tasks()->get();
 
-            dd($tasks);
+            $completed = 0;
+            $total = 0;
 
+            foreach ($tasks as $task) {
+                if($task->completed) {
+                    $completed++;
+                }
+                $total++;
+            }
+
+            $response['completed'] = $completed;
+            $response['total'] = $total;
+            $http_status_code = 200;
         } catch (\Throwable $th) {
             $response['response'] = "An error has occurred: ".$th->getMessage();
             $http_status_code = 500;
